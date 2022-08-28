a111a0d87afd231bb5461a50824f7c3181c8584c
Yang Wang <wyatuestc@gmail.com>
2022-08-28 22:02:47 +0800

    Change launch.sh wait pid

--- launch.sh
+++ launch.sh
@@ -10,11 +10,11 @@ $ADB -s $SERIAL reverse tcp:$TRACKER_PORT tcp:$TRACKER_PORT
 ssh -N -L $RPC_PORT:localhost:$RPC_PORT $SERVER &
 ssh -N -R $TRACKER_PORT:localhost:$TRACKER_PORT $SERVER &
 $PYTHON -m tvm.exec.rpc_tracker --host=0.0.0.0 --port=$TRACKER_PORT &
+pid=$!
 $PYTHON main.py --serial=$SERIAL --taskset=$TASKSET \
     --cmd-args server --host=0.0.0.0 \
     --port=$RPC_PORT --port-end=$(($RPC_PORT + 1)) \
     --tracker=127.0.0.1:$TRACKER_PORT --key=$KEY &
-pid=$!
 
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
